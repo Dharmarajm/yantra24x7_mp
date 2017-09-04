b192056f480b7704ce30a1d3fc67475cdac978bd
ubuntu <dharmarajdev6@gmail.com>
2017-09-04 18:49:19 +0530

    role commit1

--- app/usermanagement/usermanagement.js
+++ app/usermanagement/usermanagement.js
@@ -17,6 +17,7 @@ angular.module('user', ['ngRoute'])
   $scope.email = {
         text: 'me@example.com'
       };
+  $scope.emailFormat = '/^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/';    
 $scope.tenant_id=localStorage.getItem('tenant_id');
 $rootScope.tenant_id=$scope.tenant_id;
 /*$scope.role_type_id=localStorage.getItem("role_type_id");
@@ -25,7 +26,7 @@ $scope.reference_id=localStorage.getItem("reference_id");*/
 
  
 $scope.userregistration = {
-                          "id": null,
+                          "id":null,
                           "first_name": '',
                           "last_name": '',
                           "email": '',
@@ -45,7 +46,7 @@ $scope.userregistration = {
                           "city": '',
                           "state": '',
                           "country":'',
-                          "pincode": '',
+                          "pin_code": '',
                           "approval_status":'active',
                           "role_id":null,
                           "tenant_id": $scope.tenant_id
@@ -68,7 +69,7 @@ $scope.userForm= function(){
                                    "city": $scope.userregistration.city,
                                    "state": $scope.userregistration.state,
                                    "country":$scope.userregistration.country,
-                                   "pincode": $scope.userregistration.pincode, 
+                                   "pin_code": $scope.userregistration.pin_code, 
                                    "usertype_id":$scope.userregistration.usertype_id,
                                    "approval_status":$scope.userregistration.approval_status,
                                    "role_id":$scope.userregistration.role_id,
@@ -111,18 +112,53 @@ $scope.userregistrationform="";
         alert('Registration Failed');   
         }
       });
-    }else
-    {
+  }
+
+}    
       
- $http
-      ({
+ 
+
+$scope.userUpdateForm=function(){
+ var userregistrationform = {
+                              "user":{ 
+                                   
+                                   "first_name":$scope.userregistration.first_name,
+                                   "last_name":$scope.userregistration.last_name,
+                                   "email":$scope.userregistration.email,
+                                   "password":$scope.userregistration.password,
+                                   "phone_one": $scope.userregistration.phone_one,
+                                   "phone_two": $scope.userregistration.phone_two,
+                                   "addtional_detail":$scope.userregistration.addtional_detail,
+                                   "address_one": $scope.userregistration.address_one,
+                                   "address_two": $scope.userregistration.address_two,
+                                   "city": $scope.userregistration.city,
+                                   "state": $scope.userregistration.state,
+                                   "country":$scope.userregistration.country,
+                                   "pin_code": $scope.userregistration.pin_code, 
+                                   "usertype_id":$scope.userregistration.usertype_id,
+                                   "approval_status":$scope.userregistration.approval_status,
+                                   "role_id":$scope.userregistration.role_id,
+                                   "tenant_id": $scope.tenant_id
+                                   },
+
+                              "user_detail":{     
+                                   "adhar_card_no": $scope.userregistration.adhar_card_no,
+                                   "driving_license_no": $scope.userregistration.driving_license_no,
+                                   "acc_no": $scope.userregistration.acc_no,
+                                   "acc_name": $scope.userregistration.acc_name,
+                                   "bank_name": $scope.userregistration.bank_name,
+                                   "branch_name": $scope.userregistration.branch_name,
+                                   "ifsc_code": $scope.userregistration.ifsc_code,
+                                   }
+                            };
+ $http({
         method: 'put',
-        url: $rootScope.api_url+'api/v1/users/'+$scope.userregistration.id,
+        url: $rootScope.api_url+'users/'+$scope.userregistration.id,
         data: userregistrationform  
       })
       
       .success(function(data) {
-        
+        console.log($scope.success=data);
         if(data){
 
        // $state.go('/company_registration');
@@ -133,20 +169,8 @@ alert("Updated Successfully");
         }
       });
 
-    }
-
-    }
-
-$http({
-
-    method:'GET',
-    url:$rootScope.api_url+'tenant_roles?tenant_id='+$scope.tenant_id+'&&reference_id='+$scope.reference_id+'&&role_type_id='+$scope.role_type_id
-  })
-  .then(function(response){
-   $rootScope.role_ids = response.data; 
-       console.log($rootScope.role_ids);
-         $rootScope.count=$rootScope.role_ids.length
-    })
+    
+}
 
 $scope.userinit=function(){
 
@@ -160,19 +184,55 @@ $http({
    $rootScope.users = response.data; 
    
     })
+
+$http({
+
+    method:'GET',
+    url:$rootScope.api_url+'tenant_roles?tenant_id='+$scope.tenant_id+'&&reference_id='+$scope.reference_id+'&&role_type_id='+$scope.role_type_id
+  })
+  .then(function(response){
+   $rootScope.role_ids = response.data; 
+       console.log($rootScope.role_ids);
+         $rootScope.count=$rootScope.role_ids.length
+    })
 }
 
   $scope.cleandata=function(){
 
-$scope.cleardata= {id: null,first_name:"",last_name:"",email_id:"",password:"",phone_number:"", remarks: "",usertype_id: 1,approval_id: 1,role_id:null,tenant_id: $scope.tenant_id};
+$scope.cleardata= {
+                          "id":null,
+                          "first_name": '',
+                          "last_name": '',
+                          "email": '',
+                          "password": '',
+                          "phone_one": '',
+                          "phone_two": '',
+                          "adhar_card_no": '',
+                          "driving_license_no": '',
+                          "acc_no": '',
+                          "acc_name": '',
+                          "bank_name": '',
+                          "branch_name": '',
+                          "ifsc_code": '',
+                          "addtional_detail":'',
+                          "address_one": '',
+                          "address_two": '',
+                          "city": '',
+                          "state": '',
+                          "country":'',
+                          "pin_code": '',
+                          "approval_status":'active',
+                          "role_id":null,
+                          "tenant_id": $scope.tenant_id
+                         };
 $scope.userregistration = angular.copy($scope.cleardata);
   }
 
 
 
 
-    $scope.edit = function(id) {
-var i;
+$scope.edit = function(id) {
+  var i;
    for(i in $rootScope.users) {
 
             if($rootScope.users[i].id == id) {
